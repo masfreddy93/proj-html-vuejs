bce711e882342567711f694c0213f912d205a072
masfreddy93 <masieronicola@gmail.com>
2022-09-14 15:14:29 +0200

    add HeaderComponent to App

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <div class="container">
+            ciao
+        </div>
+    </header>
+</template>
+
+
+<script>
+
+    export default {
+
+    }
+
+</script>
+
+
+
+<style lang="scss">
+
+    @import '../style/variables.scss';
+
+    header {
+        background-color: $header-bg;
+        color: white;
+
+
+    }
+
+</style>
