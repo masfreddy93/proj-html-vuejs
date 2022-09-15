2dda06323c7d7c4f4a44117720f5f9d0610a5441
masfreddy93 <masieronicola@gmail.com>
2022-09-15 09:38:03 +0200

    create SocialIconsComponent

---
+++ src/components/SocialIconsComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <li>
+        <a :href="icon.href">
+            <font-awesome-icon :icon="icon.iconText" />
+        </a>
+    </li>
+</template>
+
+
+<script>
+export default {
+    props: {
+        icon: Object
+    },
+}
+</script>
