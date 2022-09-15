f5d34034ae7e373b17305b00f14b3b5474ece628
masfreddy93 <masieronicola@gmail.com>
2022-09-15 09:38:45 +0200

    create HeaderStart component

---
+++ src/components/HeaderStart.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header-start">
+        <figure class="logo">
+            <a :href="logo.href"><img :src="logo.src" alt=""></a>
+        </figure>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: {
+        logo: Object,
+    }
+}
+</script>
