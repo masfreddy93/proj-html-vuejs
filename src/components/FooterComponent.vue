eb6b226a9360c94acbbe190a28bcca907d22e5ce
masfreddy93 <masieronicola@gmail.com>
2022-09-15 09:40:21 +0200

    refactoring

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,18 +2,8 @@
     <footer>
         <div class="container">
             <div class="footer-top">
-                <div class="footer-top-left">
-                    <h4> {{ contactDetails.id }} </h4>
-                    <address> {{ contactDetails.address}} </address>
-                    <p> {{ contactDetails.telephone }} [{{ contactDetails.hours }}] </p>
-                    <p> {{ contactDetails.email }} </p>
-                    <ul class="social-icons">
-                        <li v-for="(icon, i) in socialIcons" :key="i" :class="icon.class"><a :href="icon.href"> <font-awesome-icon :icon="icon.iconText" /> </a></li>
-                    </ul>
-                </div>
-                <div class="footer-top-right">
-                    dx
-                </div>
+                <FooterTopLeftComponent :contactDetails="contactDetails" />
+                <FooterTopRightComponent />
             </div>
 
             <div class="footer-bottom">
@@ -25,93 +15,101 @@
 
 
 <script>
+import FooterTopLeftComponent from './FooterTopLeftComponent.vue';
+import FooterTopRightComponent from './FooterTopRightComponent.vue';
 
-    export default{
-        data() {
-            return {
-                contactDetails: {
-                    id: 'Address',
-                    address: '382 NE 199 St 3487 Miami FL 33209-2899',
-                    telephone: '+1(305)547-9909',
-                    hours: '9am - 5pm EST, Monday-Friday',
-                    email: 'support@maxcoach.com'
-                },
-                socialIcons: [
-                    {
-                        class: 'facebook-icon',
-                        iconText: 'fa-brands fa-square-facebook',
-                        href: '#'
-                    },
-                    {
-                        class: 'twitter-icon',
-                        iconText: 'fa-brands fa-twitter',
-                        href: '#'
-                    },
-                    {
-                        class: 'instagram-icon',
-                        iconText: 'fa-brands fa-instagram',
-                        href: '#'
-                    },
-                    {
-                        class: 'linkedin-icon',
-                        iconText: 'fa-brands fa-linkedin',
-                        href: '#'
-                    },
-                ],
-                copyright: '© 2020 Maxcoach. All Rights Reserved'
+export default {
+    data() {
+        return {
+            contactDetails: {
+                id: "Address",
+                address: "382 NE 199 St 3487 Miami FL 33209-2899",
+                telephone: "+1(305)547-9909",
+                hours: "9am - 5pm EST, Monday-Friday",
+                email: "support@maxcoach.com"
+            },
 
-            }
-        }
-    }
+            copyright: "© 2020 Maxcoach. All Rights Reserved"
+        };
+    },
+    components: { FooterTopLeftComponent, FooterTopRightComponent }
+}
 
 </script>
 
 
 
 <style lang="scss">
-    @import '../style/variables.scss';
+@import '../style/variables.scss';
 
-    footer {
-        background-color: $footer-bg-grey;
-        
-        .footer-top {
-            padding: 4.25rem 0;
-            @include my-display-flex;
-            justify-content: flex-start;
-            
-            .footer-top-left {
-                flex-basis: 0;
-                flex-grow: 1;
+footer {
+    background-color: $footer-bg-grey;
 
-                h4, address, p {
-                    margin-bottom: 1.5rem;
-                    font-size: 0.75rem;
-                }
+    .footer-top {
+        padding: 4.25rem 0;
+        @include my-display-flex;
+        justify-content: flex-start;
 
-                h4 {
-                    font-size: 1rem;
-                }
+        .footer-top-left {
+            flex-basis: 0;
+            flex-grow: 1;
 
-                .social-icons {
-                    padding-top: 0.75rem;
-                    @include my-display-flex;
-                    justify-content: flex-start;
-                    gap: 1.5rem;
-                }
+            h4, address, p {
+                margin-bottom: 1.5rem;
+                font-size: 0.75rem;
             }
 
-            .footer-top-right {
-                flex-basis: 0;
-                flex-grow: 1;
+            h4 {
+                font-size: 1rem;
+            }
+
+            .social-icons {
+                padding-top: 0.75rem;
+                @include my-display-flex;
+                justify-content: flex-start;
+                gap: 1.5rem;
+                color: $font-color-grey-section;
             }
         }
 
-        .footer-bottom {
-            margin-bottom: 3.75rem 0;
-            font-size: 0.75rem;
-            color: $font-color-grey-section;
-            text-align: center;
+        .footer-top-right {
+            flex-basis: 0;
+            flex-grow: 1;  
+            @include my-display-flex;
+            justify-content: flex-start;
+            align-items: flex-start;
+
+            & > div {
+                flex-basis: 0;
+                flex-grow: 1;
+            }
+            
+            .footer-top-right-explore ul {
+                @include my-display-flex;
+                flex-wrap: wrap;
+
+                li {
+                    flex-basis: calc(50% - 0.5rem);
+                }
+            }
+
+            h4, li {
+                margin-bottom: 1.5rem;
+                font-size: 0.75rem;
+            }
+
+            h4 {
+                font-size: 1rem;
+            }
+
         }
     }
 
+    .footer-bottom {
+        font-size: 0.75rem;
+        color: $font-color-grey-section;
+        text-align: center;
+        padding-bottom: 3.75rem;
+    }
+}
 </style>
