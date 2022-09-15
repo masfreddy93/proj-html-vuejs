35b39e569d30378d770204555d838af4dfeef7d9
masfreddy93 <masieronicola@gmail.com>
2022-09-15 09:38:25 +0200

    create InformationComponent

---
+++ src/components/InformationComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <li>
+        <a :href="information.href"> {{ information.text }} </a>
+    </li>
+</template>
+
+
+<script>
+export default {
+    props: {
+        information: Object,
+    }
+}
+</script>
