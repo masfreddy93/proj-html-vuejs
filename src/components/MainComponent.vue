35a3a36ca01e719440e164e5ffa22ce9beb2b160
masfreddy93 <masieronicola@gmail.com>
2022-09-15 13:36:42 +0200

    add section4 layout

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -163,6 +163,77 @@
                 </ul>
             </div>
         </section>
+        <section class="section-4">
+            <div class="container">
+                <h5>Enjoy reading on maxcoach</h5>
+                <h2>Latest On <mark>Our Blogs</mark></h2>
+                <ul class="row-section">
+                    <li>
+                        <div class="card">
+                            <figure class="card-thumbnail">
+                                <a href="#"><img src="../assets/images/artist-blog-03-480x325.jpeg" alt=""></a>
+                            </figure>
+                            <div class="card-details">
+                                <h5>Artist</h5>
+                                <p><a href="#">Bush Strokes Energize Trees in Paintings</a></p>
+                                <div class="blog-details row-section">
+                                    <div class="article-date row-section">
+                                        <font-awesome-icon icon="fa-regular fa-calendar" /> 
+                                        <span>May 15 2020</span>
+                                    </div>
+                                    <div class="views-number row-section">
+                                        <font-awesome-icon icon="fa-regular fa-eye" />
+                                        <span>650 Views</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="card">
+                            <figure class="card-thumbnail">
+                                <a href="#"><img src="../assets/images/artist-blog-01-480x325.jpg" alt=""></a>
+                            </figure>
+                            <div class="card-details">
+                                <h5>Artist</h5>
+                                <p><a href="#">Pocket Sized Notebooks Hold Miniature Paintings</a></p>
+                                <div class="blog-details row-section">
+                                    <div class="article-date row-section">
+                                        <font-awesome-icon icon="fa-regular fa-calendar" />
+                                        <span>May 15 2020</span>
+                                    </div>
+                                    <div class="views-number row-section">
+                                        <font-awesome-icon icon="fa-regular fa-eye" />
+                                        <span>603 Views</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="card">
+                            <figure class="card-thumbnail">
+                                <a href="#"><img src="../assets/images/artist-blog-02-480x325.jpg" alt=""></a>
+                            </figure>
+                            <div class="card-details">
+                                <h5>Artist</h5>
+                                <p><a href="#">Connection Between Self-Portraits and Identity</a></p>
+                                <div class="blog-details row-section">
+                                    <div class="article-date row-section">
+                                        <font-awesome-icon icon="fa-regular fa-calendar" />
+                                        <span>May 15 2020</span>
+                                    </div>
+                                    <div class="views-number row-section">
+                                        <font-awesome-icon icon="fa-regular fa-eye" />
+                                        <span>397 Views</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
     </main>
 </template>
 
@@ -292,5 +363,49 @@ main {
             gap: 0.5rem;
         }
     }
+
+    .section-4 {
+        background-color: $section-bg-grey;
+
+        h5 {
+            color: $font-color-grey-section;
+        }
+
+        ul {
+            gap: 2rem;
+
+            .card-thumbnail {
+                margin-bottom: 1.5rem;
+            }
+
+            h5 {
+                color: $font-color-grey-section;
+                margin-bottom: 1rem;
+                padding: 0;
+                font-size: 1rem;
+            }
+
+            p {
+                margin-bottom: 2rem;
+            }
+
+            p a {
+                line-height: 2rem;
+                font-size: 1.4375rem;
+                font-weight: bold;
+            }
+
+            .blog-details {
+                // gap: 1.5rem;
+
+                .row-section {
+                    gap: .5rem;
+                    font-size: 0.75rem;
+                }
+            }
+
+        }
+    }
+
 }
 </style>
